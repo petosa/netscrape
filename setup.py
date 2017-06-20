110f0fee9b6184a35436a996fb056c1a038ce191
Nick Petosa <nick.petosa@gmail.com>
2017-06-20 03:15:53 -0400

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author='nick petosa',
     author_email='nick.petosa@gmail.com',
     description='Automated, scheduled, intelligent web scraping server and client.',
-    install_requires=['pytest==3.0.6', 'pymongo==3.3.0', "testfixtures==5.1.1", "Flask=0.11.1", "Flask-RESTful=0.3.6"]
+    install_requires=['pytest==3.0.6', 'pymongo==3.3.0', "testfixtures==5.1.1", "Flask==0.11.1", "Flask-RESTful==0.3.6"]
 )
